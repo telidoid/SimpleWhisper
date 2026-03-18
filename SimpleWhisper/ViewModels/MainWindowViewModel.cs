01f2a73debfec6523279ec18a2af9af768ddaf76
telidoid <like_coder@proton.me>
2026-03-18 19:56:11 +0500

    Show version number in window title

--- SimpleWhisper/ViewModels/MainWindowViewModel.cs
+++ SimpleWhisper/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -6,6 +7,8 @@ namespace SimpleWhisper.ViewModels;
 public partial class MainWindowViewModel(MainPageViewModel mainPage, ModelsPageViewModel modelsPage, SettingsPageViewModel settingsPage) : ViewModelBase
 {
     [ObservableProperty] private ViewModelBase _currentPage = mainPage;
+
+    public string Title { get; } = $"SimpleWhisper v{(typeof(MainWindowViewModel).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "?").Split('+')[0]}";
 
     public MainPageViewModel MainPage { get; } = mainPage;
     public ModelsPageViewModel ModelsPage { get; } = modelsPage;
